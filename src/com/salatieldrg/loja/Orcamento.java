898b23ca6375afb320e0b55397f53e88b5cc9ba5
Salatiel dos Reis <salatieldrg@gmail.com>
2022-08-30 12:08:53 -0300

    criação projeto

---
+++ src/com/salatieldrg/loja/Orcamento.java
@@ -0,0 +1,16 @@
+package com.salatieldrg.loja;
+
+import java.math.BigDecimal;
+
+public class Orcamento {
+	private BigDecimal valor;
+
+	public Orcamento(BigDecimal valor) {
+		super();
+		this.valor = valor;
+	}
+	
+	public BigDecimal getValor() {
+		return valor;
+	}
+}
